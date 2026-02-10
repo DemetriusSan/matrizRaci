2176ceb3b6d40e9ba75d40fc84ffd3770cda987f
Desan_12 <demluagab@gmail.com>
2026-02-10 09:41:41 -0300

    feat(home, microfocus): implement home and microfocus components with routing and styles

--- matrizRaci/src/app/app.routes.ts
+++ matrizRaci/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { RACIMatrixComponent } from './features/raci/raci-matrix.component';
 import { Tutorial } from './features/tutorial/tutorial';
+import { Home } from './features/home/home';
+import { Microfocus } from './features/microfocus/microfocus';
 
 export const routes: Routes = [
-  { path: '', component: RACIMatrixComponent },
+  { path: '', component: Home },
+  { path: 'microfoco', component: Microfocus },
+  { path: 'raci', component: RACIMatrixComponent },
   { path: 'tutorial', component: Tutorial }
 ];
---
+++ matrizRaci/src/app/features/microfocus/microfocus.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-microfocus',
+  imports: [RouterLink],
+  templateUrl: './microfocus.html',
+  styleUrl: './microfocus.scss',
+})
+export class Microfocus {}
